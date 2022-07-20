de8b078bcf9f597291571391efbb11946a9c6182
sajieun <sajieun000721@naver.com>
2022-07-20 15:46:15 +0900

    dd

---
+++ app/src/main/java/com/cookandroid/bottomnavigation/FreeList.java
@@ -0,0 +1,14 @@
+package com.cookandroid.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_list);
+    }
+}
